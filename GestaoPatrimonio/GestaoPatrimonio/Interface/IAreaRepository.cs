109edb9f103de622eaa2c43d0a7f109a5da777e2
guirrs <guirsan6os189@gmail.com>
2026-03-24 10:52:36 -0300

    Feat: Inicio API

---
+++ GestaoPatrimonio/GestaoPatrimonio/Interface/IAreaRepository.cs
@@ -0,0 +1,9 @@
+﻿using GestaoPatrimonio.Domains;
+
+namespace GestaoPatrimonio.Interface
+{
+    public interface IAreaRepository
+    {
+        public List<Area> Listar();
+    }
+}
---
+++ GestaoPatrimonio/GestaoPatrimonio/Repositories/AreaRepository.cs
@@ -0,0 +1,20 @@
+﻿using GestaoPatrimonio.Contexts;
+using GestaoPatrimonio.Domains;
+using GestaoPatrimonio.Interface;
+
+namespace GestaoPatrimonio.Repositories
+{
+    public class AreaRepository : IAreaRepository
+    {
+        private readonly GestaoPatrimoniosContext _context;
+
+        public AreaRepository(GestaoPatrimoniosContext context)
+        {
+            _context = context;
+        }
+        public List<Area> Listar()
+        {
+            return _context.Area.OrderBy(area => area.NomeArea).ToList();
+        }
+    }
+}
